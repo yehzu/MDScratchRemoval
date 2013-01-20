c7c77e46687a185b5d406cda5792ff57ffa8a0cd
yehzu <yehzu2@gmail.com>
2013-01-20 20:12:06 +0800

    load image and sharp filted

--- multi_dir_scratch_detection.m
+++ multi_dir_scratch_detection.m
@@ -8,8 +8,28 @@ function multi_dir_scratch_detection(img)
 	bluredImg = imfilter(img, h);
 	sharpImg = img - bluredImg;
 
-	imshow(sharpImg);
+	DEBUG_SHOW(sharpImg); 
+
 	% apply band-pass filter (Kass and Witkin [5])
+	% this substep generates 12 binary image
+	% hard to understand...OMG????
 	
 
+	% 12 binary image are stored in cell array
+	
 
+	%%%% step 2: Line Direction Detection
+	
+	for b_img_idx = 1:12
+
+
+	end
+	%%%% step 3: Contour Drawing
+
+
+	%%%% step 4: Core Detection
+
+
+function DEBUG_SHOW(img)
+	imshow(img)
+	pause(1);
