6e12ceed235fd7be06a79f2f752bc3650b98f7b5
yehzu <yehzu2@gmail.com>
2013-01-21 17:16:04 +0800

    Use matlab build-in hougth line dectector

--- multi_dir_scratch_detection.m
+++ multi_dir_scratch_detection.m
@@ -1,7 +1,7 @@
-function multi_dir_scratch_detection(img)
+function out =  multi_dir_scratch_detection(img)
 
 % needs to be tuned
-max_scratch_num = 20;
+max_scratch_num = 4;
 scratch_threshold = 50;
 
 
@@ -11,9 +11,9 @@ scratch_threshold = 50;
 	% get sharp part of img
 	h = fspecial('gaussian');
 	bluredImg = imfilter(img, h);
-	sharpImg = img - bluredImg;
-
-	DEBUG_SHOW(sharpImg); 
+	sharpImg = double(img - bluredImg);
+	sharpImg = sharpImg / max(sharpImg(:));
+	DEBUG_SHOW(sharpImg);
 
 	% apply band-pass filter (Kass and Witkin [5])
 	% this substep generates 12 binary image
@@ -21,41 +21,52 @@ scratch_threshold = 50;
 	
 
 	% 12 binary image are stored in cell array
-	band_pass_imgs
-	bp_imgs_num = 12;
+	band_pass_imgs{1} = im2bw(sharpImg, mean(sharpImg(:)));
+	bp_imgs_num = 1;
+
+	DEBUG_SHOW(band_pass_imgs{1})
 	
+
+
+
 	%%%% step 2: Line Direction Detection
-	accumulator = 0;
+	% accumulator = 0;
 	for bp_imgs_idx = 1:bp_imgs_num
-		h_img = hough(band_pass_imgs{bp_imgs_idx});
-		accumulator = accumulator + h_img;
-	end
-
-	% find scratches
-	idx_record = zeros(2, max_scratch_num);
-	num_points_record = zeros(1, max_scratch_num);
-	for detect_scratch = 1:max_scratch_num
-		[max_idx_m, max_idx_n, num_points]= find(accumulator == max(accumulator));
-		
-		if (detect_scratch == 1) || (num_points_record(detect_scratch - 1) - num_points < scratch_threshold)
-			idx_record(:, detect_scratch) = [max_idx_m, max_idx_n];
-			num_points_record(detect_scratch) = num_points;
-			
-			% neighborhood values are set to zero to prevent multiple overlapping scratches
-			% % % % % % % % % % %
-			% didn't implemented
-			accumulator(max_idx_m, max_idx_n) = 0;
-			
+		[h_img theta rho] = hough(band_pass_imgs{bp_imgs_idx});
+		if bp_imgs_idx == 1
+			accumulator = h_img;
 		else
-			break;
+			accumulator = accumulator + h_img;
 		end
-		
+	end
+	
+	DEBUG_SHOW(accumulator/max(accumulator(:)));
+	
+	
+	% find scratches
+	P = houghpeaks(accumulator, max_scratch_num,'threshold', ceil(0.3*max(accumulator(:))))
+	lines = houghlines(band_pass_imgs{1}, theta, rho, P, 'FillGap', 10, 'MinLength', 7);
+	figure, imshow(img), hold on
+	max_len = 0;
+	for k = 1:length(lines)
+		xy = [lines(k).point1; lines(k).point2];
+		plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+
+		% Plot beginnings and ends of lines
+		plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+		plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+
+		% Determine the endpoints of the longest line segment
+		len = norm(lines(k).point1 - lines(k).point2);
+		if ( len > max_len)
+			 max_len = len;
+			 xy_long = xy;
+		end
 	end
 
-
-
 	
-	
+
+out = 0;
 	%%%% step 3: Contour Drawing
 
 
@@ -64,4 +75,4 @@ scratch_threshold = 50;
 
 function DEBUG_SHOW(img)
 	imshow(img)
-	pause(1);
+	waitforbuttonpress
