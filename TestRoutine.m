cb588bfcd019c2c7a451d356325f1ab9d2a5b942
yehzu <yehzu2@gmail.com>
2013-02-20 15:41:45 +0800

    add test image

--- TestRoutine.m
+++ TestRoutine.m
@@ -1,4 +1,8 @@
+ I = imread('test.jpg');
+% I = imread('20060503165139725.jpg' );
+% I = imread('a13425342_s.jpg');
+% I = imread('1.jpg');
+% I = imread('SITDOWN7.JPG');
 
-I = imread('1.jpg');
 detected = multi_dir_scratch_detection(I);
 
