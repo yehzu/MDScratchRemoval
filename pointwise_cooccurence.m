264d7fc108ce67d1ca4b30eaa44ae28fc25056a5
yehzu <yehzu2@gmail.com>
2013-03-26 19:36:15 +0800

    use integral image to acce.

--- pointwise_cooccurence.m
+++ pointwise_cooccurence.m
@@ -12,42 +12,66 @@ end
 out = zeros(M, N);
 
 k = 1;
-h = waitbar(0,'Computing');
 
+DP_table = zeros([M, N + 1, 4]);
 
-f = [];
+h = waitbar(0, 'Estabulish DP table');
 
-if isempty(f);
-    f = zeros(M*N, 4);
-
-    for jj = 1:N
-        for ii = 1:M
-
-            iidx = max(ii - nbrsize, 1) : min(ii + nbrsize, M);
-            jidx = max(jj - nbrsize, 1) : min(jj + nbrsize, N);
-
-            glcm = graycomatrix(img(iidx, jidx));
-
-            f(k, :) = glcm(:);
-            k = k+1;
+fprintf(1, 'building table\n');
+for jj = 2:(N+1)
+    for ii = 2:M
+        
+        % initial the table
+        DP_table(ii, jj, :) = DP_table(ii - 1, jj, :) + DP_table(ii, jj - 1, :) - DP_table(ii-1, jj-1, :);
+       
+        
+        sub = img(ii, jj - 1) - img(ii - 1, jj - 1);
+        mul = img(ii, jj - 1) * img(ii - 1, jj - 1);
+        
+        
+        switch sub * 2 - mul
+            case 0   % [0 0]
+                idx = 1;
+            case -1  % [1 1]
+                idx = 4;
+            case -2  % [0 1]
+                idx = 2;
+            case 2   % [1 0]
+                idx = 3;
         end
-        waitbar(k/(M*N), h)
+
+        % update
+        DP_table(ii, jj, idx) = DP_table(ii, jj, idx) + 1;
+        
     end
+    waitbar(jj * M / (M*N), h);
 end
-    
-    
 close(h)
 
-idx = [];
 
+
+% cooccurence matrix
+f = zeros(M*N, 4);
+for jj = 1:N
+    for ii = 1:M
+        glcm =   DP_table(min(ii + nbrsize, M), min(jj + nbrsize, N) + 1, :) ...
+               - DP_table(max(ii - nbrsize, 1), min(jj + nbrsize, N) + 1, :) ...
+               - DP_table(min(ii + nbrsize, M), max(jj - nbrsize, 1), :) ...
+               + DP_table(max(ii - nbrsize, 1), max(jj - nbrsize, 1), :);
+
+        f(k, :) = glcm(:);
+        k = k+1;
+    end
+end
 
 k = 4;
 fprintf(1, 'Clustering\n');
+
+idx = [];
 while isempty(idx)
     try
-        [idx c] = kmeans(f, k);
+        [idx ~] = kmeans(f, k);
     catch err
-    
     end
 end
 
@@ -56,8 +80,8 @@ for i = 1:k
    val = img(idx == i);
    v(i) = sqrt(var(val(:))) / mean(val(:)); % coefficient of variance
 end
+
 [~, sid] = sort(v);
-v
 out(idx ~= sid(3)) = 1;
 
 end
