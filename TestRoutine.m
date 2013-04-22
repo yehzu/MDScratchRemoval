b089c9aeb6cbb44136a986dc52364179e1064367
yehzu <yehzu2@gmail.com>
2013-04-22 18:58:32 +0800

    add test set

--- TestRoutine.m
+++ TestRoutine.m
@@ -7,13 +7,14 @@ w = 1;
 
 % I = imread('20060503165139725.jpg' );
 % I = imread('a13425342_s.jpg');
- I = imread('1.jpg');
+% I = imread('1.jpg');
 % I = imread('SITDOWN7.JPG');
+% I = imread('SITDOWN7-1.JPG');
 % I = imread('h.png');
 % I = imread('cup.png');
 % I = imread('blur.png');
- 
-%I = imread('testImg/t05.jpg');
+% I = imread('testImg/t05.jpg');
+I = imread('testImg/t09.jpg');
 
 detected = multi_dir_scratch_detection(I, w);
 
