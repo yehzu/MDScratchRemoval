05fe3cbf843d4167e03a853c20d319cc2d545ec0
yehzu <yehzu2@gmail.com>
2013-05-21 09:01:11 +0800

    fill gap

--- find_scratch.m
+++ find_scratch.m
@@ -8,6 +8,8 @@ min_total_scratch_pixel = 100;
 On = 1;
 Visited = 0.5;
 scratch_width = 5;
+gap_size = 10;
+fill_gap_nbr = 3;
 
 num_lines = length(lines);
 [M, N] = size(img);
@@ -25,17 +27,45 @@ for line_id = 1:num_lines
     dir = (p2 - p1) / norm(p2 - p1);
     normal = [dir(2), -dir(1)];
     
+	last_p = zeros(2, 1);
     p = p1;
     while norm(p - p2) > 1
-        
-        if sharpImg(round(p(2)), round(p(1))) == On
-            pset = extend_scratch_width(round(p), normal, scratch_width, sharpImg); % find nearest 5 pixels
-            for pt = 1:length(pset)
+        pset = extend_scratch_width(round(p), normal, scratch_width, sharpImg); % find nearest 5 pixels
+        for pt = 1:length(pset)
+		%	if sharpImg(round(p(2)), round(p(1))) == On
                 if sharpImg(pset(pt, 2), pset(pt, 1)) == On
-                    [scratches sharpImg] = grow_scratch(pset(pt, :), dir, sharpImg); % find scartch and update sharpImg
-                    point_stack(ps_size + 1: ps_size + size(scratches, 1), :) = scratches;
-                    ps_size = ps_size + size(scratches, 1);
-                end
+					
+                    [scratches len sharpImg] = grow_scratch(pset(pt, :), dir, sharpImg); % find scartch and update sharpImg
+                    if len > 3
+						point_stack(ps_size + 1: ps_size + size(scratches, 1), :) = scratches;
+						ps_size = ps_size + size(scratches, 1);
+
+						if sum(last_p) ~= 0 && norm(last_p - pset(pt, :)) < gap_size
+							intensity = zeros(ps_size, 1);
+							
+							for i = 1:ps_size
+								intensity(i) = img(point_stack(i, 2), point_stack(i, 1));
+							end
+							vi = var(intensity);
+							mi = mean(intensity);
+							
+							nor = norm(last_p - pset(pt, :));
+
+							pt1 = round(linspace(last_p(1), pset(pt, 1), nor))
+							pt2 = round(linspace(last_p(2), pset(pt, 2), nor))
+							
+							for i = 1:length(pt1)
+								if abs(img(pt2(i), pt1(i)) - mi) < 2* vi
+									fprintf(1, 'fill scratch\n');
+									point_stack(ps_size + 1, :) = [pt1(i), pt2(i)];
+									ps_size = ps_size + 1;
+								end
+							end
+							
+						end
+						last_p = scratches(end, :);
+					end
+         %       end
             end
         end
         p = p + dir;
@@ -43,7 +73,6 @@ for line_id = 1:num_lines
     % len = [0:1:norm(p2 - p1), norm(p2 - p1)]; % small bug (twice norm(p2 - p1))
     
 end
-point_stack(1:ps_size, :)
 for pt = 1:ps_size	
 	out(point_stack(pt, 2), point_stack(pt, 1)) = 1;
 end
@@ -62,18 +91,8 @@ for ptn = 1:size(pset, 1)  % remove invalid points
 end
 end
 
-%{
 %% grow_scratch: according the first p to grow the whole scratch
-function [scratches sharpImg] = grow_scratch(pset, dir, sharpImg)
-
-
-
-scratches = 1;
-%}
-
-
-%% grow_scratch: according the first p to grow the whole scratch
-function [scratches, sharpImg] = grow_scratch(p, dir, sharpImg)
+function [scratches, maxlen, sharpImg] = grow_scratch(p, dir, sharpImg)
 % parameters
 sharpImg = double(sharpImg);
 scratch_size = 1000;
@@ -93,8 +112,10 @@ n_ds = 0;
 tdir = dir / dir(1);
 % main loop
 no_scratch = false;
+maxlen = 0;
 while true
     sharpImg(round(p(2)), round(p(1))) = Visited;
+	maxlen = maxlen + 1;
 	%imshow(sharpImg)
     %waitforbuttonpress
 
@@ -108,7 +129,6 @@ while true
         rnp = round(np);
         
         if sum(rnp < 1) < 1 && rnp(2) < size(sharpImg, 1) && rnp(1) < size(sharpImg, 2) && sharpImg(rnp(2), rnp(1)) == On
-            fprintf(1, 'road 1\n');
             p = np;
             dir = tdir;
             break;
@@ -118,19 +138,16 @@ while true
             rnp1 = round(np1);
             rnp2 = round(np2);
             if sum(rnp1 < 1) < 1 && rnp1(2) < size(sharpImg, 1) && rnp1(1) < size(sharpImg, 2) && sharpImg(rnp1(2), rnp1(1)) == On
-                fprintf(1, 'road 2\n');
                 p = np1;
                 dir = tdir;
                 break;
                 
             elseif sum(rnp2 < 1) < 1 && rnp2(2) < size(sharpImg, 1) && rnp2(1) < size(sharpImg, 2) && sharpImg(rnp2(2), rnp2(1)) == On
-                    fprintf(1, 'road 3\n');
                     p = np2;
                     dir = tdir;
                     break;
             else
                     no_scratch = true;
-                    fprintf(1, 'road 4\n');
             end
                 %theta = (rand(1) * 2 - 1) * pi / 5;
                 %theta
@@ -139,14 +156,12 @@ while true
         end % end if
         test = test + 1;
         if test > rand_test_time || no_scratch
-            fprintf(1, 'break\n');
             break;
         end % end if
             
     end % end while     
         
     if test > rand_test_time || no_scratch
-        fprintf(1, 'break\n');
         break;
     end % end if
 
